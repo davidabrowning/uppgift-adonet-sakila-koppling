e8c05434abdf0a7ca7084767d032fb410b97d2f8
David Browning <david.a.browning@gmail.com>
2025-04-07 16:12:41 +0200

    Build menu flow.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Huvudmeny");
+                Console.WriteLine("1. Sök efter skådespelarens förnamn");
+                Console.WriteLine("2. Sök efter skådespelarens efternamn");
+                Console.WriteLine("3. Sök efter både förnamn och efternamn");
+                Console.WriteLine("4. Lista ut alla skådespelare");
+                Console.WriteLine("5. Avsluta programmet");
+
+                switch(Console.ReadLine().Trim())
+                {
+                    case "1":
+                        Console.Write("Ange förnamn: ");
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.Write("Ange efternamn: ");
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        Console.Write("Ange förnamn: ");
+                        Console.ReadLine();
+                        Console.Write("Ange efternamn: ");
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        Console.Write("Listar ut alla skådespelare");
+                        Console.ReadLine();
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Oväntad inmatning. Försök igen.");
+                        break;
+                }
+            }
         }
     }
 }
