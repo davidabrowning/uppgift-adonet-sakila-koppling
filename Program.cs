d71d1791881aad4c7a6ee5a673b841e406257f42
David Browning <david.a.browning@gmail.com>
2025-04-07 16:56:27 +0200

    Add first and last name queries. Move menu option display to separate method.

--- Program.cs
+++ Program.cs
@@ -8,29 +8,29 @@ namespace ADOnetSakilaKoppling
         {
             while(true)
             {
-                Console.Clear();
-                Console.WriteLine("Huvudmeny");
-                Console.WriteLine("1. Sök efter skådespelarens förnamn");
-                Console.WriteLine("2. Sök efter skådespelarens efternamn");
-                Console.WriteLine("3. Sök efter både förnamn och efternamn");
-                Console.WriteLine("4. Lista ut alla skådespelare");
-                Console.WriteLine("5. Avsluta programmet");
+                ClearAndShowMenuOptions();
 
                 switch(Console.ReadLine().Trim())
                 {
                     case "1":
                         Console.Write("Ange förnamn: ");
+                        string firstName = Console.ReadLine().Trim();
+                        PrintQueryResults($"SELECT * FROM actor WHERE first_name LIKE '{firstName}'");
                         Console.ReadLine();
                         break;
                     case "2":
                         Console.Write("Ange efternamn: ");
+                        string lastName = Console.ReadLine().Trim();
+                        PrintQueryResults($"SELECT * FROM actor WHERE last_name LIKE '{lastName}'");
                         Console.ReadLine();
                         break;
                     case "3":
                         Console.Write("Ange förnamn: ");
-                        string firstName = Console.ReadLine().Trim();
+                        string firstName2 = Console.ReadLine().Trim();
                         Console.Write("Ange efternamn: ");
-                        string lastName = Console.ReadLine().Trim();
+                        string lastName2 = Console.ReadLine().Trim();
+                        PrintQueryResults($"SELECT * FROM actor WHERE first_name LIKE '{firstName2}' AND last_name LIKE '{lastName2}'");
+                        Console.ReadLine();
                         break;
                     case "4":
                         Console.Write("Listar ut alla skådespelare");
@@ -44,6 +44,16 @@ namespace ADOnetSakilaKoppling
                         break;
                 }
             }
+        }
+        public static void ClearAndShowMenuOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("Huvudmeny");
+            Console.WriteLine("1. Sök efter skådespelarens förnamn");
+            Console.WriteLine("2. Sök efter skådespelarens efternamn");
+            Console.WriteLine("3. Sök efter både förnamn och efternamn");
+            Console.WriteLine("4. Lista ut alla skådespelare");
+            Console.WriteLine("5. Avsluta programmet");
         }
         public static void PrintQueryResults(string query)
         {
