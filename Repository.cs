c943c61bdb64ee627fd6c8af0e83b410a058f533
David Browning <david.a.browning@gmail.com>
2025-04-09 16:53:17 +0200

    Wrap db connections in using statements.

--- Repository.cs
+++ Repository.cs
@@ -20,19 +20,23 @@ namespace ADOnetSakilaKoppling
 
             using (var connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Sakila;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"))
             {
-                var command = new SqlCommand(query, connection);
                 connection.Open();
-                var result = command.ExecuteReader();
-                if (result.HasRows)
+                using (var command = new SqlCommand(query, connection))
                 {
-                    while (result.Read())
+                    using (var result = command.ExecuteReader())
                     {
-                        string[] newResult = new string[result.FieldCount];
-                        for (int i = 0; i < result.FieldCount; i++)
+                        if (result.HasRows)
                         {
-                            newResult[i] = result[i].ToString();
+                            while (result.Read())
+                            {
+                                string[] newResult = new string[result.FieldCount];
+                                for (int i = 0; i < result.FieldCount; i++)
+                                {
+                                    newResult[i] = result[i].ToString();
+                                }
+                                results.Add(newResult);
+                            }
                         }
-                        results.Add(newResult);
                     }
                 }
                 connection.Close();
