94f82e0b0e4a00322612f0109817d67de5f4caf6
David Browning <david.a.browning@gmail.com>
2025-04-09 19:43:25 +0200

    Create Film class.

---
+++ Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOnetSakilaKoppling
+{
+    internal class Film
+    {
+        public string Title { get; }
+        public Film(string title)
+        {
+            Title = title;
+        }
+    }
+}
