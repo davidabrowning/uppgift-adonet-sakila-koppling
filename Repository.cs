a7bcfd29cdf8b00f6684600a8c8731a1eb7d86e8
David Browning <david.a.browning@gmail.com>
2025-04-10 11:14:29 +0200

    Route parameter-less queries through same overall query method using an empty parameter list.

--- Repository.cs
+++ Repository.cs
@@ -11,42 +11,10 @@ namespace ADOnetSakilaKoppling
 {
     internal class Repository
     {
-        public List<string[]> GetQueryResults(string query)
+        public List<string[]> GetQueryResultsWithoutParameters(string query)
         {
-            List<string[]> results = new List<string[]>();
-
-            string appsettings = File.ReadAllText("Appsettings.json");
-            JsonDocument appsettingsJson = JsonDocument.Parse(appsettings);
-            string connectionString = appsettingsJson
-                .RootElement
-                .GetProperty("ConnectionStrings")
-                .GetProperty("Sakila")
-                .ToString();
-
-            using (var connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                using (var command = new SqlCommand(query, connection))
-                {
-                    using (var result = command.ExecuteReader())
-                    {
-                        if (result.HasRows)
-                        {
-                            while (result.Read())
-                            {
-                                string[] newResult = new string[result.FieldCount];
-                                for (int i = 0; i < result.FieldCount; i++)
-                                {
-                                    newResult[i] = result[i].ToString();
-                                }
-                                results.Add(newResult);
-                            }
-                        }
-                    }
-                }
-                connection.Close();
-            }
-            return results;
+            List<string[]> emptyParameterList = new List<string[]>();
+            return GetQueryResults(query, emptyParameterList);
         }
         public List<string[]> GetQueryResults(string query, List<string[]> parameters)
         {
@@ -90,7 +58,7 @@ namespace ADOnetSakilaKoppling
         public List<Actor> GetActors(string actorQuery)
         {
             List<Actor> actors = new List<Actor>();
-            foreach (string[] actorResult in GetQueryResults(actorQuery))
+            foreach (string[] actorResult in GetQueryResultsWithoutParameters(actorQuery))
             {
                 int.TryParse(actorResult[0], out int actorId);
                 actors.Add(new Actor(actorId, actorResult[1], actorResult[2]));
@@ -116,7 +84,7 @@ namespace ADOnetSakilaKoppling
                     $"INNER JOIN film_actor ON film_actor.film_id = film.film_id " +
                     $"INNER JOIN actor ON actor.actor_id = film_actor.actor_id " +
                     $"WHERE actor.actor_id = {actor.ActorId}";
-                List<string[]> filmResults = GetQueryResults(filmQuery);
+                List<string[]> filmResults = GetQueryResultsWithoutParameters(filmQuery);
                 foreach (string[] filmResult in filmResults)
                 {
                     int.TryParse(filmResult[0], out int filmId);
