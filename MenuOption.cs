0a3ad53b35e6fbbe6ea084ef5775bc8ca25a84fd
David Browning <david.a.browning@gmail.com>
2025-04-11 16:41:46 +0200

    Create MenuOption enum.

---
+++ MenuOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOnetSakilaKoppling
+{
+    internal enum MenuOption
+    {
+        SearchByFirstName = 1,
+        SearchByLastName = 2,
+        SearchByFullName = 3,
+        ListAllActors = 4,
+        Exit = 5
+    }
+}
