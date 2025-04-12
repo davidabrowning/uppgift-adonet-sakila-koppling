1896f7736569e33d99180acf9ec5215bd8beff4b
David Browning <david.a.browning@gmail.com>
2025-04-12 19:15:02 +0200

    Create MethodToCall class.

---
+++ Services/MenuOptionClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOnetSakilaKoppling.Services
+{
+    internal class MenuOptionClass
+    {
+        public string Title { get; private set; }
+        public Action MethodToCall { get; private set; }
+        public MenuOptionClass(string title, Action methodToCall)
+        {
+            Title = title;
+            MethodToCall = methodToCall;
+        }
+    }
+}
