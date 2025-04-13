b92865a0b035c8598b6e0c9fc4e46f749ef841b5
David Browning <david.a.browning@gmail.com>
2025-04-13 23:46:38 +0200

    Update missing using.

--- ApplicationLauncher.cs
+++ ApplicationLauncher.cs
@@ -1,4 +1,5 @@
-﻿using ADOnetSakilaKoppling.Interfaces;
+﻿using ADOnetSakilaKoppling.Database;
+using ADOnetSakilaKoppling.Interfaces;
 using ADOnetSakilaKoppling.Menus;
 using ADOnetSakilaKoppling.Repositories;
 using ADOnetSakilaKoppling.Services;
@@ -18,7 +19,7 @@ namespace ADOnetSakilaKoppling
             IOutput output = new ConsoleOutput();
             IInput input = new Keyboard(output);
             IConnectionStringBuilder connectionStringBuilder = new SakilaConnectionStringBuilder();
-            IDbAccess dbAccess = new SakilaDbAccess(connectionStringBuilder);
+            IDbAccess dbAccess = new DbAccess(connectionStringBuilder);
             IQueryBuilder queryBuilder = new ActorFilmQueryBuilder();
             IActorFilmRepository actorFilmRepository = new ActorFilmRepository(queryBuilder, dbAccess);
             IActorFilmService actorFilmService = new DataService(input, output, actorFilmRepository);
