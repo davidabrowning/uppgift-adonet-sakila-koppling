4b5a0f057dbde15dd90cf6ac7887c61900cb2770
David Browning <david.a.browning@gmail.com>
2025-04-13 16:24:48 +0200

    Remove GetFilmsByFirstName methods.

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -10,7 +10,6 @@ namespace ADOnetSakilaKoppling.Interfaces
     internal interface IRepository
     {
         List<Actor> GetActorsByField(List<ActorMapping> actorMappings);
-        List<Actor> GetActorsByFirstName(string firstName);
         List<Actor> GetActorsByLastName(string lastName);
         List<Actor> GetActorsByFullName(string firstName, string lastName);
         List<Actor> GetAllActors();
